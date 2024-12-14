0195df4d960327c2ae599c368482f992d747b215
Arghya <arghya.official@yahoo.com>
2024-12-14 07:04:39 +0530

    made some changest

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -1,15 +1,15 @@
 exports.homepage = async (req, res) => {
   const locals = {
-    title: "NodeJs Notes",
-    description: " Free NodeJs Notes App",
+    title: "CodeCaddy",
+    description: " CodeCaddy App",
   };
   res.render("index", { locals, layout: "layouts/front-page" });
 };
 
 exports.about = async (req, res) => {
   const locals = {
-    title: "About - NodeJs Notes",
-    description: " Free NodeJs Notes App",
+    title: "About - CodeCaddy App",
+    description: " CodeCaddy App",
   };
   res.render("about", locals);
 };
